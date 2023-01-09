b7f855d7f4094d005009bf8f5e0b80df08dd6a99
Mayank Jobanputra <mayankjobanputra@gmail.com>
2023-01-09 13:56:20 +0530

    first draft

---
+++ airgapped-rest_api/test_docker.sh
@@ -0,0 +1,4 @@
+curl --request POST --url http://0.0.0.0:8000/query \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data '{"query": "what is my name"}'
