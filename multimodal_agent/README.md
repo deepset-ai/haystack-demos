1f1d88826ccbd5dce73a78c5537f0b8a9da597ce
bilgeyucel <bilgeyucel96@gmail.com>
2025-09-16 15:28:39 +0200

    add demo files

---
+++ multimodal_agent/README.md
@@ -0,0 +1,110 @@
+# 🖼️ Multimodal Haystack Agent
+This demo shows how to build a multimodal agent that can interact with images and PDFs using [Haystack](https://haystack.deepset.ai/), [Hayhooks](https://github.com/deepset-ai/hayhooks), and [Open WebUI](https://docs.openwebui.com/).
+
+## 🚀 Run the Demo
+
+### 1️⃣ Clone the Repository
+```sh
+git clone git@github.com:deepset-ai/haystack-demos.git
+cd haystack-demos/multimodal_agent
+```
+### 2️⃣ Install Dependencies(Python>=3.10)
+Create and activate a virtual environment (optional but recommended):
+```sh
+python3.11 -m venv .venv
+source .venv/bin/activate
+```
+Install Haystack, Hayhooks and other required packages:
+```sh
+pip install haystack-ai python-weather hayhooks google-genai-haystack 
+```
+
+### 3️⃣ Configure Environment Variables
+Set API Keys:
+```sh
+export OPENAI_API_KEY=your_openai_api_key # For OpenAI models
+```
+
+🔗 **More details on configuration:** [Hayhooks Documentation](https://github.com/deepset-ai/hayhooks?tab=readme-ov-file#configuration)
+
+### 4️⃣ Start the Hayhooks Server 
+```sh
+hayhooks run
+```
+Check if Hayhooks is running:
+```sh
+hayhooks status
+```
+Output:
+```sh
+╭─────────────────────────────────────────────────────────────╮
+│ Hayhooks server is up and running at: http://localhost:1416 │
+╰─────────────────────────────────────────────────────────────╯
+
+No pipelines currently deployed
+```
+
+### 5️⃣ Deploy the Agent
+Choose the agent that you want to deploy and use the corresponding path:
+```sh
+hayhooks pipeline deploy-files -n basic-agent multimodal_agent/basic-agent
+```
+
+```sh
+hayhooks pipeline deploy-files -n agent-with-tool multimodal_agent/agent-with-tool
+```
+If deployment is successful, you'll see output like this when you run `hayhooks status` again:
+```sh
+╭─────────────────────────────────────────────────────────────╮
+│ Hayhooks server is up and running at: http://localhost:1416 │
+╰─────────────────────────────────────────────────────────────╯
+
+        Deployed Pipelines         
+╭───┬─────────────────┬───────────╮
+│ № │ Pipeline Name   │ Status    │
+├───┼─────────────────┼───────────┤
+│ 1 │ agent-with-tool │ 🟢 Active │
+╰───┴─────────────────┴───────────╯
+```
+
+After making changes on the `pipeline_wrapper.py` files, you can redeploy the same agent with the `--overwrite` command without restarting the Hayhooks server.
+```sh
+hayhooks pipeline deploy-files -n agent-with-tool --overwrite multimodal_agent/agent-with-tool
+```
+
+### 6️⃣ Test the API
+Swagger docs: [http://localhost:1416/docs](http://localhost:1416/docs)
+
+Test with `curl`:
+```sh
+curl -X 'POST' \
+  'http://localhost:1416/agent-with-tool/run' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "query": "What's the weather like in this place?",
+  "image_path": "multimodal_agent/map.png"
+}'
+```
+
+## 🖥️ Connect to Open WebUI
+
+### 1️⃣ Start Open WebUI
+**Official Quick Start:** [Open WebUI Docs](https://docs.openwebui.com/)
+
+Run Open WebUI using Docker:
+```sh
+docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway \
+  -e WEBUI_AUTH=False -v open-webui:/app/backend/data \
+  --name open-webui ghcr.io/open-webui/open-webui:main
+```
+⚠️ **Disables authentication.** If you have Open WebUI authentication enabled, delete existing Docker volumes before running this command.
+
+Once running, open: [http://localhost:3000/](http://localhost:3000/). If you're having problems with Open WebUI, remove your browser cache and cookies.
+
+### 2️⃣ Connect Open WebUI to the REST API
+1. Go to **Settings** → **Admin Settings** → **Connections** in the Open WebUI
+2. Add `http://host.docker.internal:1416` to OpenAI API connections, fill in a placeholder string as "Key" 
+3. Select a pipeline as the "model" for chat
+
+![Add Connection](add-ui-connection.png)
---
+++ multimodal_agent/agent-with-tool/pipeline_wrapper.py
@@ -0,0 +1,98 @@
+import asyncio
+import python_weather
+from typing import AsyncGenerator, Annotated, Optional, List
+
+from haystack.components.agents import Agent
+from haystack.dataclasses import ChatMessage, ImageContent
+from haystack.components.generators.chat import OpenAIChatGenerator
+from haystack.components.generators.utils import print_streaming_chunk
+from haystack.tools import tool
+
+from hayhooks import BasePipelineWrapper, async_streaming_generator
+from hayhooks.open_webui import OpenWebUIEvent, create_notification_event, create_status_event, create_details_tag
+
+class PipelineWrapper(BasePipelineWrapper):
+    def setup(self) -> None:
+        # Initialize tool
+        @tool
+        def get_weather(location: Annotated[str, "The location to get the weather for"]) -> dict:
+            """A function to get the weather for a given location"""
+            async def _fetch_weather():
+                async with python_weather.Client(unit=python_weather.METRIC) as client:
+                    weather = await client.get(location)
+                    return {
+                        "description": weather.description,
+                        "temperature": weather.temperature,
+                        "humidity": weather.humidity,
+                        "precipitation": weather.precipitation,
+                        "wind_speed": weather.wind_speed,
+                        "wind_direction": weather.wind_direction
+                    }
+
+            return asyncio.run(_fetch_weather())
+        
+        self.agent = Agent(
+            chat_generator=OpenAIChatGenerator(model="gpt-4o-mini"),
+            system_prompt="You're a helpful agent",
+            tools=[get_weather]
+        )
+    
+    # Deploy Tool Calling Agent 
+    def run_api(self, query: str, image_path: Optional[str] = None) -> str:
+        if image_path:
+            image = ImageContent.from_file_path(image_path)
+            content_parts = [query, image]
+        else:
+            content_parts = [query]
+        result = self.agent.run([ChatMessage.from_user(content_parts=content_parts)], streaming_callback=print_streaming_chunk)
+        return result["last_message"].text
+    
+    def on_tool_call_start(
+        self, tool_name: str, arguments: dict, id: str
+    ) -> List[OpenWebUIEvent]:
+        return [
+            # create_status_event(description=f"Tool call started: {tool_name}"),
+            # create_notification_event(
+            #     notification_type="info",
+            #     content=f"Tool call started: {tool_name}",
+            # )
+        ]
+    
+    def on_tool_call_end(
+        self,
+        tool_name: str,
+        arguments: dict,
+        result: str,
+        error: bool,  # noqa: ARG002
+    ) -> list[OpenWebUIEvent]:
+        return [
+            # create_status_event(
+            #     description=f"Tool call ended: {tool_name} with arguments:{arguments}",
+            #     done=True,
+            # ),
+            # create_notification_event(
+            #     notification_type="success",
+            #     content=f"Tool call ended: {tool_name}",
+            # ),
+            # create_details_tag(
+            #     tool_name=tool_name,
+            #     summary=f"Tool call result for {tool_name}",
+            #     content=(f"```\nArguments:\n{arguments}\n\nResponse:\n{result}\n```"),
+            # ),
+        ]
+    
+    # handles OpenAI-compatible chat completion requests asynchronously for Open WebUI
+    async def run_chat_completion_async(
+        self, model: str, messages: list[dict], body: dict
+    ) -> AsyncGenerator[str, None]:
+        chat_messages = [
+            ChatMessage.from_openai_dict_format(message) for message in messages
+        ]
+        return async_streaming_generator(
+            on_tool_call_start=self.on_tool_call_start,
+            on_tool_call_end=self.on_tool_call_end,
+            pipeline=self.agent,
+            pipeline_run_args={
+                "messages": chat_messages,
+            },
+        )
